0e97ea0e2cd6e2426e09c276f709233bfc149c7a
Ubuntu <ubuntu@ip-172-31-0-98.us-west-2.compute.internal>
2023-02-23 02:55:05 +0000

    added movie to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,9 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'Interstellar', :rating => 'PG-13',
+    :release_date => '26-Oct-2014'}
 ]
 
 more_movies.each do |movie|
